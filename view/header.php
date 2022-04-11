100295ff9ff06552a7ac6209679b40dfb8ae7bbd
tougmaa <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-04-11 20:44:13 +0200

    css

--- view/header.php
+++ view/header.php
@@ -15,8 +15,8 @@
 					<li><a href="inscription.ctrl.php">S'inscrire</a></li>
 					<li><a href="#">Se connecter</a></li>
 					<li><a href="#">Catalogue</a></li>
-					<li><a href="#">Comments book</a></li>
-					<li><a href="#">Who we are</a></li>
+					<li><a href="#">Panier</a></li>
+					<li><a href="#">Historique</a></li>
       </ul>
       </nav>
   </header>
