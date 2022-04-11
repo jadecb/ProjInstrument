d7eaae59a97d9cf3b6083048f4767db521178aa9
tougmaa <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-04-11 13:42:05 +0200

    class et DAO

---
+++ model/accessoireDAO.class.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(dirname(__FILE__.'/accessoire.class.php'));
+
+// Data Access Object pour Accessoire
+class AccessoireDAO{
+
+}
+
+?>
---
+++ model/instrumentDAO.class.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(dirname(__FILE__.'/instrument.class.php'));
+
+// Data Access Object pour Instrument
+class InstrumentDAO{
+
+}
+
+?>
