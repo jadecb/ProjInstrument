264f9683e6cf268398062143db156ab88041e36e
jade <jadeclemenceaub@hotmail.com>
2022-04-11 18:14:06 +0200

    Modification constructeur Client DAO pour accéder au données

--- model/clientDAO.class.php
+++ model/clientDAO.class.php
@@ -4,14 +4,7 @@ require_once(dirname(__FILE__.'/client.class.php'));
 
 // Data Access Object pour Client
 class ClientDAO{
-function __construct() {
- try {
- $this->db = new PDO('sqlite:data/Sibemol.db');
- if (!$this->db) { die ("Database error"); }
- } catch (PDOException $e) {
- die("PDO Error :".$e->getMessage()."$this->database\n");
-    }
-  }
+
 }
 
 ?>
