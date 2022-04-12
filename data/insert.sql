3f6d1e1d564276d4bfce4a6ce061d26d7aeea27e
Rémy <remy.albanese2@gmail.com>
2022-04-12 17:00:52 +0200

    insert.sql

---
+++ data/insert.sql
@@ -0,0 +1,92 @@
+/***************/
+/******BOIS*****/
+/***************/
+
+
+/* ACCORDEONS */
+
+insert into infoArticle values (1,'Accordéon rouge-argenté',50);
+insert into infoInstrument values (1,'bois', 'rouge', 45,75,30, 'bois');
+insert into Accordeon values (1,37);
+
+insert into infoArticle values (6, 'Accordéon en bois chêne', 75);
+insert into infoInstrument values (6, 'bois', 'chene', 45,75,30,'bois');
+insert into Accordeon values (6, 37);
+
+insert into infoArticle values (11, 'Accordéon en bois enfant',25);
+insert into infoInstrument values(11, 'bois', 'chene', 45,75,30,'bois');
+insert into Accordeon values(11,24);
+
+/* FLUTES */
+
+insert into infoArticle values (16,'Flute à bec blanche en bois', 15);
+insert into infoInstrument values (16, 'bois', 'blanc',25,4,4,'bois');
+insert into Flute values (16, 'flute a bec',8);
+
+insert into infoArticle values (21, 'Flute à bec en bois chêne',20);
+insert into infoInstrument values (21, 'bois','chêne',25,4,4,'bois');
+insert into Flute values (21, 'flute a bec',8);
+
+insert into infoArticle values (26, 'Flute à bec en bois enfant marron et blanche',10);
+insert into infoInstrument values (26, 'bois','marron',25,4,4,'bois');
+insert into Flute values (26, 'flute a bec',8);
+
+
+insert into infoArticle values (31, 'Flute de pan en bamboo', 55);
+insert into infoInstrument values (31, 'bamboo', 'blanc',20,20,4,'bois');
+insert into Flute values (31, 'flute de pan',20);
+
+insert into infoArticle values (36, 'Flute de pan en bamboo blanc décoré enfant', 20);
+insert into infoInstrument values (36, 'bamboo', 'blanc',13,13,6,'bois');
+insert into Flute values (36, 'flute de pan',13);
+
+insert into infoArticle values (41, 'Flute de pan en bamboo chêne', 34);
+insert into infoInstrument values (41, 'bamboo', 'chene',16,16,4,'bois');
+insert into Flute values (41, 'flute de pan',15);
+
+
+insert into infoArticle values (46, 'Flute traversière argentée',850);
+insert into infoInstrument values (46, 'argent', 'argent', 40,5,5, 'bois');
+insert into Flute values (46, 'flute traversiere',16);
+
+insert into infoArticle values (51, 'Flute traversière argentée',450);
+insert into infoInstrument values (51, 'argent', 'argent', 40,5,5, 'bois');
+insert into Flute values (51, 'flute traversiere',16);
+
+insert into infoArticle values (56, 'Flute traversière argentée',699);
+insert into infoInstrument values (56, 'argent', 'argent', 40,5,5, 'bois');
+insert into Flute values (56, 'flute traversiere',16);
+
+
+/* HARMONICA */
+
+insert into infoArticle values (61, 'Harmonica argent trois couleurs',45);
+insert into infoInstrument values (61, 'argent','argent',12,5,3,'bois');
+insert into Harmonica values (61, 'harmonica',10);
+
+insert into infoArticle values (66, 'Harmonica argent deux couleurs',30);
+insert into infoInstrument values (66, 'argent','argent',12,5,3,'bois');
+insert into Harmonica values (66, 'harmonica',10);
+
+insert into infoArticle values (71, 'Harmonica argenté doré',69);
+insert into infoInstrument values (71, 'argent','argent',12,5,3,'bois');
+insert into Harmonica values (71, 'harmonica',10);
+
+/* SAXOPHONE */
+
+insert into infoArticle values (76, 'Saxophone cuivre-doré',469);
+insert into infoInstrument values(76,'cuivre','doré',80,40,30,'bois');
+insert into Saxophone values (76, 'saxophone', 19);
+
+insert into infoArticle values (81, 'Saxophone cuivre-doré',649);
+insert into infoInstrument values(81,'cuivre','doré',80,40,30,'bois');
+insert into Saxophone values (81, 'saxophone', 21);
+
+insert into infoArticle values (86, 'Saxophone cuivre-doré',1149);
+insert into infoInstrument values(86,'cuivre','doré',80,40,30,'bois');
+insert into Saxophone values (86, 'saxophone', 22);
+
+
+/******************/
+/******CLAVIER*****/
+/******************/
