5a082cf6e4acf4adb122f3199c74e0c09c3a33b2
Rémy <remy.albanese2@gmail.com>
2022-04-12 13:22:23 +0200

    Formulaires css

--- view/connect.view.php
+++ view/connect.view.php
@@ -1,22 +1,27 @@
 <?php require("../view/header.php"); ?>
 
 <main>
+
+    <form action="t_connect.ctrl.php" method="get">
     <?php if(isset($nouvelementInscrit)) : ?>
         <p>Inscription bien enregistrée, vous pouvez vous connecter !</p>
     <?php endif; ?>
-
-    <form action="t_connect.ctrl.php" method="get">
-        <label for="mail">Mail : </label>
-        <input type="email" name="mail" required/>
-        <label for="mdp">Mot de passe (8 characters minimum) : </label>
-        <input type="password" name="mdp" minlength="8" required/>
+        <fieldset>
+            <label for="mail">Mail</label>
+            <input type="email" name="mail" required/>
+            <label for="mdp">Mot de passe (8 characters minimum)</label>
+            <input type="password" name="mdp" minlength="8" required/>
         
-        <input type="submit" value="CONNECTION">
+            <input type="submit" value="CONNEXION">
+        </fieldset>
         <p><a href="inscription.ctrl.php">Je n'ai pas de compte</a></p>
+
+        <?php if(isset($mauvaisId)) : ?>
+        <p></br>Mauvais mail ou mot de passe</p>
+        <?php endif; ?>
+
     </form>
-    <?php if(isset($mauvaisId)) : ?>
-        <p>Mauvais mail ou mot de passe</p>
-    <?php endif; ?>
+
 </main>
 
 <?php require("../view/footer.php"); ?>
--- view/inscription.view.php
+++ view/inscription.view.php
@@ -2,20 +2,22 @@
 
 <main>
     <form action="t_inscription.ctrl.php" method="get">
-        <label for="prenom">Prénom : </label>
+        <fieldset>
+        <label for="prenom">Prénom</label>
         <input type="text" name="prenom" required/>
-        <label for="nom">Nom : </label>
+        <label for="nom">Nom</label>
         <input type="text" name="nom" required/>
-        <label for="adresse">Adresse :</label>
+        <label for="adresse">Adresse</label>
         <input type="text" name="adresse" required/>
         <label for="dateNaissance">Date de naissance</label>
         <input type="date" name="dateNaissance" required/>
-        <label for="mail">Mail : </label>
+        <label for="mail">Mail</label>
         <input type="email" name="mail" required/>
-        <label for="mdp">Mot de passe (8 characters minimum) : </label>
+        <label for="mdp">Mot de passe (8 characters minimum)</label>
         <input type="password" name="mdp" minlength="8" required/>
-        
         <input type="submit" value="INSCRIPTION">
+        </fieldset>
+
         <p><a href="connect.ctrl.php">J'ai déjà un compte</a></p>
 
         <?php if(isset($mailExistant)) : ?>
