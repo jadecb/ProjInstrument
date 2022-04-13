c1a269d1f2f9cd4fc496ca951ff890df024faabf
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-04-13 17:18:09 +0200

    catalogue.ctrl ok

--- view/catalogue.view.php
+++ view/catalogue.view.php
@@ -1,15 +1,16 @@
-<? php require("../view/header.php"); ?>
+<?php require("../view/header.php"); ?>
 
 <main>
-     action="t_catalogue.ctrl.php" method="get"
     <?php foreach ($allInstruments as $instrument): ?>
-    <figure>
-    <a href="t_catalogue.ctrl.php?instrument=<?=$instrument>"> <img src="../images/catalogue/<?=$instrument?>.jpg"> </a>
-    <figcaption>
-    <?=$instrument?>
-    </figcaption>
-    </a>
-    </figure>
+        <figure>
+            <a href="t_catalogue.ctrl.php?instrument=<?=$instrument?>">
+                <img src="../images/catalogue/<?=$instrument?>.jpg" width="100px" height="100px">
+            
+                <figcaption>
+                    <?=$instrument?>
+                </figcaption>
+            </a>
+        </figure>
     <?php endforeach; ?>
 </main>
 
