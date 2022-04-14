f710747ff63cd91b4ad5eb4bcc326b4be7417a40
Arthur_Tougma <arthur.tougma@etu.univ-grenoble-alpes.fr>
2022-04-14 09:28:39 +0200

    affiche un instrument choisi

---
+++ controler/afficheInstrument.ctrl.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once(__DIR__.'/../config.php');
+require_once(__DIR__.'/../framework/view.fw.php');
+
+/* *** PARTIE RECUPARATION DES DONNEES *** */
+if(isset($_SESSION['prenom'])){
+    $prenom = $_SESSION['prenom'];
+}
+
+// Récupération du type d'instrumment voulant etre affiché
+// si non trouvé renvoi au controleur précédent
+if(isset($_GET['instrument']) && isset($_GET['numArticle'])){
+    $instrument = $_GET['instrument'];
+    $numArticle = $_GET['numArticle'];
+}
+else{
+    header('Location: catalogue.ctrl.php');
+}
+
+
+// ajout de la classDAO de l'instrument choisi
+require_once(__DIR__.'/../model/'.$instrument.'DAO.class.php');
+/* *** PARTIE USAGE DU MODELE *** */
+
+// DAO
+$DAO = $instrument.'DAO';
+$DAO = new $DAO();
+
+$method = 'get'.ucfirst($instrument);
+$$instrument = $DAO->$method();
+
+require_once('../model/InfoInstrumentAttributs.php');
+require_once('../model/InstrumentsAttributs');
+$instrumentAttributs = $InstrumentsAttributs[$instrument];
+
+/* *** GESTION DE LA VUE *** */
+
+$view = new View();
+if(isset($_SESSION['prenom'])){
+    $view->assign('prenom',$prenom);
+}
+$view->assign('instrument',$instrument);
+$view->assign('tabInfosInstrument',$$instrument);
+$view->assign('InfoInstrumentAttributs',$InfoInstrumentAttributs);
+$view->assign('instrumentAttributs',$instrumentAttributs);
+$view->display('afficheInstrument.view.php');
+
+?>
--- view/afficheInstrument.view.php
+++ view/afficheInstrument.view.php
@@ -2,13 +2,21 @@
 
 <main id="afficheInstrument">
         <div>
-                <img src="../images/<?=$instrument?>/<?=$tabInfosInstrument['numArticle']?>.jpg">
-                <div>
-                    <h2><?=$tabInfosInstrument['nom']?></h2>
-                    <p>Prix : <?=$tabInfosInstrument['prix']?> €</p>
-                </div>
+            <img src="../images/<?=$instrument?>/<?=$tabInfosInstrument['numArticle']?>.jpg">
+            <div>
+                <h2><?=$tabInfosInstrument['nom']?></h2>
+                <p>Prix : <?=$tabInfosInstrument['prix']?> €</p>
+            </div>
+            <table>
+                <?php foreach($InfoInstrumentAttributs as $attribut): ?>
+                    <tr><td><?=ucfirst($attribut)?></td><td><?=$tabInfosInstrument[$attribut]?></td></tr>
+                <? endforeach; ?>
+
+                <?php foreach($instrumentAttributs as $key => $value): ?>
+                    <tr><td><?=ucfirst($key)?></td><td><?=$tabInfosInstrument[$key]?></td></tr>
+                <? endforeach; ?>
+            </table>
         </div>
 </main>
-
 
 <?php require("../view/footer.php"); ?>
