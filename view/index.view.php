ed1883f9f41c0c21e07140cd168c409c76341b80
Rémy <remy.albanese2@gmail.com>
2022-04-14 09:24:49 +0200

    changement index

--- view/index.view.php
+++ view/index.view.php
@@ -1,9 +1,24 @@
-<?php require("../view/header.php"); ?>
+<?php require("../view/header.php"); 
+$a = null;
+?>
 
-<main>
+<main id="grostexte">
     <h1>
-        contenu principal
+        Sibémol    
     </h1>
+    <p>Producteur d'instruments de musique depuis 2007</p>
+    <p>Découvrez nos produits phares :</p>
+    <div  id="mainpage">
+    <?php for($i=0; $i<3; $i++): ?>
+        <figure>
+            <a href="t_catalogue.ctrl.php?instrument=<?=$allInstruments[$i]?>">
+                <img src="../images/catalogue/<?=$allInstruments[$i]?>.jpg" alt="image catalogue">
+            <figcaption>
+                <?=$allInstruments[$i]?>
+            </figcaption>
+            </a></figure>
+    <?php endfor; ?>
+    </div>
 </main>
 
 <?php require("../view/footer.php"); ?>
