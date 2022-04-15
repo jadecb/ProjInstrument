ad3a46a83e7a4ceeea2e9ac0c0a55cd4c6551e2e
jadecb <jadeclemenceaub@hotmail.com>
2022-04-15 12:21:48 +0200

    page d'acceuil css html

--- view/catalogue.view.php
+++ view/catalogue.view.php
@@ -1,29 +1,34 @@
-<?php require("../view/header.php"); ?>
-
 <main id="catalogue">
-
-    <figure>
-        <a href="t_catalogue.ctrl.php?choix=accessoire">
-            <img src="../images/accessoire.jpg">
-            
-            <figcaption>
-                Accessoires
-            </figcaption>
-        </a>
-    </figure>
-
-    <figure>
-        <a href="t_catalogue.ctrl.php?choix=instrument">
-            <img src="../images/instruments.jpg">
-            
-            <figcaption>
-                Instruments de musique
-            </figcaption>
-        </a>
-    </figure>
-
-
-
+    <section class="scroll-parent">
+        <nav class="dots">
+                <a href="#first" class="dot"></a>
+                <a href="#second" class="dot"></a>
+        </nav>
+            <!-- Enfant -->  
+            <div id="second">
+                <figure>
+                <a href="t_catalogue.ctrl.php?choix=instrument">
+                    <img src="../images/instruments.jpg">
+                    
+                    <figcaption>
+                        Instruments de musique
+                    </figcaption>
+                </a>
+                </figure>
+            </div> 
+            <!-- Enfant -->  
+            <div id="first">
+                <figure>
+                <a href="t_catalogue.ctrl.php?choix=accessoire">
+                    <img src="../images/accessoire.jpg">
+                    
+                    <figcaption>
+                        Accessoires
+                    </figcaption>
+                </a>
+                </figure>
+            </div>
+    </section>
 </main>
 
 
